52b0499075e83348ce36fd99cd8a91b115e036ba
jy19Phy <49807120+jy19Phy@users.noreply.github.com>
2024-12-21 15:34:36 +0800

    Add files via upload

---
+++ Fig2/Nq5/My_rhocheck.py
@@ -0,0 +1,53 @@
+# import os
+# os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import math
+from os import stat
+import numpy as np
+import torch 
+import random
+from torch import nn
+from torch.nn  import functional as F 
+from torch.autograd import Variable
+import pennylane as qml
+from torch.optim import Adam
+
+
+from Myrho import generat_random_densitymatrix_fromdia, generate_pure_density_matrix
+from My_train import train_fun
+from Mymonitor import time_string_fun
+
+
+
+
+if __name__ == '__main__':
+	print("=========================================================================================\n")
+
+	statetime= time_string_fun()
+
+	Nq = 4
+
+	rho_matrix = torch.load("./Data/rho_N"+str(Nq)+".pt")
+
+
+
+	purity = torch.real(torch.einsum( 'ij,ji', rho_matrix , rho_matrix ))
+	print("System purity=",purity)
+	
+
+	eigE = torch.linalg.eigvals(rho_matrix)
+	eigE_sort , index= torch.sort(torch.real(eigE))
+	print('EigE',eigE_sort ,"\n")
+
+	rho_hermi = torch.transpose( torch.conj( rho_matrix), 0,1  )
+
+	diff = torch.sum(torch.square(rho_matrix - rho_hermi) ) 
+	print("hermi diff=", diff)
+
+
+
+	
+	
+
+
+
+	
